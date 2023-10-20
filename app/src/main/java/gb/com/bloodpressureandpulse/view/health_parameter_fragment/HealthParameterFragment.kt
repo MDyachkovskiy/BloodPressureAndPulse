b5dcf6ab925a42712cab4c792f11626e6fd8b5b2
MDyachkovskiy <115916173+MDyachkovskiy@users.noreply.github.com>
2023-10-20 17:23:48 +0500

    - созданый макеты для отображения пульса и давления

---
+++ app/src/main/java/gb/com/bloodpressureandpulse/view/health_parameter_fragment/HealthParameterFragment.kt
@@ -0,0 +1,34 @@
+package gb.com.bloodpressureandpulse.view.health_parameter_fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import gb.com.bloodpressureandpulse.databinding.FragmentHealthParameterBinding
+
+
+class HealthParameterFragment : Fragment() {
+
+    private var _binding: FragmentHealthParameterBinding? = null
+    private val binding get() = _binding!!
+
+    companion object {
+        fun newInstance() = HealthParameterFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentHealthParameterBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/gb/com/bloodpressureandpulse/view/main/MainActivity.kt
@@ -0,0 +1,33 @@
+package gb.com.bloodpressureandpulse.view.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import com.google.android.material.snackbar.Snackbar
+import gb.com.bloodpressureandpulse.R
+import gb.com.bloodpressureandpulse.databinding.ActivityMainBinding
+import gb.com.bloodpressureandpulse.view.health_parameter_fragment.HealthParameterFragment
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.main_container, HealthParameterFragment.newInstance())
+            .commit()
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAnchorView(R.id.fab)
+                .setAction("Action", null).show()
+        }
+    }
+}
