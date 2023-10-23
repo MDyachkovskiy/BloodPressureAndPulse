0e4ba14231ff0fe829a54bcb8d4980a9c0849292
MDyachkovskiy <115916173+MDyachkovskiy@users.noreply.github.com>
2023-10-23 14:47:47 +0500

    - добавлен функционал изменения background в зависимости от значений давления

--- app/src/main/java/gb/com/bloodpressureandpulse/view/health_parameter_fragment/adapter/InnerAdapter.kt
+++ app/src/main/java/gb/com/bloodpressureandpulse/view/health_parameter_fragment/adapter/InnerAdapter.kt
@@ -2,7 +2,9 @@ package gb.com.bloodpressureandpulse.view.health_parameter_fragment.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import gb.com.bloodpressureandpulse.R
 import gb.com.bloodpressureandpulse.databinding.ItemBloodPressurePulseBinding
 import gb.com.bloodpressureandpulse.model.domain.VitalSigns
 import java.text.SimpleDateFormat
@@ -22,12 +24,31 @@ class InnerAdapter : RecyclerView.Adapter<InnerAdapter.ViewHolder>() {
     ) : RecyclerView.ViewHolder(binding.root) {
         fun bind(data: VitalSigns) {
             with(binding) {
+                val systolic = data.systolicPressure
+                val diastolic = data.diastolicPressure
+
                 tvTime.text = data.date?.let {
                     SimpleDateFormat("HH:mm", Locale.getDefault()).format(it)
                 }
-                bloodPressureFirstValue.text = data.systolicPressure.toString()
-                bloodPressureSecondValue.text = data.diastolicPressure.toString()
+                bloodPressureFirstValue.text = systolic.toString()
+                bloodPressureSecondValue.text = diastolic.toString()
                 pulse.text = data.pulse.toString()
+
+                when {
+                    systolic in 110..129 -> {
+                        root.background =
+                            ContextCompat.getDrawable(root.context, R.drawable.good_vital_background)
+                    }
+                    (systolic in 130..139 ) || (systolic in 100..110) -> {
+                        root.background =
+                            ContextCompat.getDrawable(root.context, R.drawable.wary_vital_background)
+                    }
+                    (systolic >= 140 ) || (systolic <=100 ) -> {
+                        root.background =
+                            ContextCompat.getDrawable(root.context, R.drawable.alert_vital_background)
+                    }
+                    else -> {}
+                }
             }
         }
     }
